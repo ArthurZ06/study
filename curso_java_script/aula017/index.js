9c1579c4863ce230381a9aee8ce901178f185099
Arthur <arthurnicolas30@gmail.com>
2024-01-23 23:23:47 -0300

    commit

---
+++ curso_java_script/aula017/index.js
@@ -0,0 +1,13 @@
+function soma(x = 0, y = 0) {
+  const resultado = x + y;
+  return resultado;
+}
+
+const resultado = soma(3, 7);
+console.log(resultado);
+
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(4).toFixed(2));
+console.log(raiz(8).toFixed(2));
+console.log(raiz(16).toFixed(2));

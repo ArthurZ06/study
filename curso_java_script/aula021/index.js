0919b275d1319ab3e1443494ee6c45eacb2e037f
Arthur <arthurnicolas30@gmail.com>
2024-01-27 01:36:49 -0300

    commit

---
+++ curso_java_script/aula021/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação 
+> maior que 
+>= maior que ou igual a 
+< menor que 
+<= menor que ou igual a
+=== igual estrita (valor e tipo)
+!== igual estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = "10";
+const resultado = num1 === num2;
+console.log(resultado);
